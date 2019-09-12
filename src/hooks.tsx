60c71696dbbbf5064f84cf05a82764bd35bb5b6a
Maarten Hus <maarten.hus@42.nl>
2019-09-12 10:26:40 +0200

    Feature: added a `useClearErrors` hook.
    It can be used to reset the errors when a form is displayed again.
    This way the errors to not linger around when the user navigates away
    and back to the form.
    
    Minor changes:
    
      1. Updated to TypeScript 3.6.3
      2. Locked dev dependencies.
      3. Made husky and lint-stage actually work.
      4. Ran `npm audit fix`

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -2,13 +2,26 @@ import { useEffect, useState } from 'react';
 
 import { errorStoreService } from './service';
 
+/**
+ * Given the validator returns the array of errors for that validator.
+ *
+ * @export
+ * @example
+ * ```js
+ *   function FormError() {
+ *      const errors = useErrorsForValidator('User.name');
+ *   }
+ * ```
+ * @param {string} validator The errors to retrieve for the validator.
+ * @returns {string[]} Array errors for that validator.
+ */
 export function useErrorsForValidator(validator: string): string[] {
   const [state, setState] = useState<string[]>([]);
 
   useEffect(() => {
     const onChange = (errors: string[]): void => {
       setState(errors);
-    }
+    };
 
     const subscriber = {
       onChange,
@@ -19,8 +32,27 @@ export function useErrorsForValidator(validator: string): string[] {
 
     return () => {
       errorStoreService.unsubscribe(subscriber);
-    }
+    };
   }, [validator]);
 
   return state;
+}
+
+/**
+ * Clears all errors from the entire store once when the component
+ * is loaded. Useful for clearing errors when a form is displayed, so
+ * old errors do not linger around.
+ *
+ * @example
+ * ```js
+ *   function UserForm() {
+ *      useClearErrors();
+ *   }
+ * ```
+ * @export
+ */
+export function useClearErrors(): void {
+  useEffect(() => {
+    errorStoreService.clearErrors();
+  });
 }
