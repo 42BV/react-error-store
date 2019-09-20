b1aae1f14c6dfbfb0adf560a1653a6108c745a57
Maarten Hus <maarten.hus@42.nl>
2019-09-20 11:34:35 +0200

    Chore: added react hooks and added np for publishing.
    Added development and release instructions.
    
    Added hooks eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,14 +11,16 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended'
   ],
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     indent: 'off',
     '@typescript-eslint/indent': ['off', 0],
     '@typescript-eslint/no-use-before-define': ['off', 0],
-    'react/prop-types': [0] // Disable propTypes warning @see https://stackoverflow.com/questions/41746028/proptypes-in-a-typescript-react-application
+    'react/prop-types': [0], // Disable propTypes warning @see https://stackoverflow.com/questions/41746028/proptypes-in-a-typescript-react-application
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
   settings: {
     react: {
--- README.md
+++ README.md
@@ -12,3 +12,23 @@ Storing errors and listening to their changes.
 # Documentation
 
 See the [documentation](https://42bv.github.io/react-error-store/).
+
+# Developing
+
+When developing a new feature, make sure you do not update the `package.json`'s
+version this will happen whenever a release is made on `master` by
+the releasers.
+
+Create a PR to submit your changes to the master branch, the maintainers
+will review your work and decide if the feature warrented or if the
+bug is vaporized.
+
+# Releasing
+
+Drafting a new release is as simple as running `npm run release`. This command runs (https://github.com/sindresorhus/np)[np] under the hood, which will guide you through the process of drafting a release.
+
+A checklist for after the release:
+
+- [] Make sure a tag is created in GitHub
+- [] Make sure the release is created in GitHub.
+- [] Make sure the release is on NPM.
