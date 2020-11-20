a9e56a382b44df5dcfb314806391586f17bb08d2
Gido Manders <gido.manders@42.nl>
2020-11-20 12:03:42 +0100

    improvement: subscribe to all errors
    To be able to display any errors, it would be useful to have the
    possibility to subscribe to all errors.
    
    Added possibility to subscribe to all errors.
    
    Closes #10

--- tests/service.test.ts
+++ tests/service.test.ts
@@ -4,11 +4,13 @@ describe('EnumsService', () => {
   test('setErrors: should set errors and inform subscribers', () => {
     const service = makeErrorStoreService();
 
-    const emailSubscriber = { onChange: jest.fn(), validator: 'User.email'};
-    const nameSubscriber = { onChange: jest.fn(), validator: 'User.name'};
+    const emailSubscriber = { onChange: jest.fn(), validator: 'User.email' };
+    const nameSubscriber = { onChange: jest.fn(), validator: 'User.name' };
+    const anySubscriber = { onChange: jest.fn() };
 
     service.subscribe(emailSubscriber);
     service.subscribe(nameSubscriber);
+    service.subscribe(anySubscriber);
 
     service.setErrors({
       User: {
@@ -21,17 +23,30 @@ describe('EnumsService', () => {
     expect(emailSubscriber.onChange).toBeCalledWith(['email is wrong']);
 
     expect(nameSubscriber.onChange).toBeCalledTimes(2);
-    expect(nameSubscriber.onChange).toBeCalledWith(['name is wrong', 'name is to short']);
+    expect(nameSubscriber.onChange).toBeCalledWith([
+      'name is wrong',
+      'name is to short'
+    ]);
+
+    expect(anySubscriber.onChange).toBeCalledTimes(2);
+    expect(anySubscriber.onChange).toBeCalledWith({
+      User: {
+        email: ['email is wrong'],
+        name: ['name is wrong', 'name is to short']
+      }
+    });
   });
 
   test('clear: should clear all errors and inform subscribers', () => {
     const service = makeErrorStoreService();
 
-    const emailSubscriber = { onChange: jest.fn(), validator: 'User.email'};
-    const nameSubscriber = { onChange: jest.fn(), validator: 'User.name'};
+    const emailSubscriber = { onChange: jest.fn(), validator: 'User.email' };
+    const nameSubscriber = { onChange: jest.fn(), validator: 'User.name' };
+    const anySubscriber = { onChange: jest.fn() };
 
     service.subscribe(emailSubscriber);
     service.subscribe(nameSubscriber);
+    service.subscribe(anySubscriber);
 
     service.setErrors({
       User: {
@@ -47,16 +62,21 @@ describe('EnumsService', () => {
 
     expect(nameSubscriber.onChange).toBeCalledTimes(3);
     expect(nameSubscriber.onChange).toHaveBeenLastCalledWith([]);
+
+    expect(anySubscriber.onChange).toBeCalledTimes(3);
+    expect(anySubscriber.onChange).toHaveBeenLastCalledWith({});
   });
 
   test('clear: should clear all errors and inform subscribers', () => {
     const service = makeErrorStoreService();
 
-    const emailSubscriber = { onChange: jest.fn(), validator: 'User.email'};
-    const nameSubscriber = { onChange: jest.fn(), validator: 'User.name'};
+    const emailSubscriber = { onChange: jest.fn(), validator: 'User.email' };
+    const nameSubscriber = { onChange: jest.fn(), validator: 'User.name' };
+    const anySubscriber = { onChange: jest.fn() };
 
     service.subscribe(emailSubscriber);
     service.subscribe(nameSubscriber);
+    service.subscribe(anySubscriber);
 
     service.setErrors({
       User: {
@@ -71,14 +91,22 @@ describe('EnumsService', () => {
     expect(emailSubscriber.onChange).toHaveBeenLastCalledWith([]);
 
     expect(nameSubscriber.onChange).toBeCalledTimes(3);
-    expect(nameSubscriber.onChange).toHaveBeenLastCalledWith(['name is wrong', 'name is to short']);
+    expect(nameSubscriber.onChange).toHaveBeenLastCalledWith([
+      'name is wrong',
+      'name is to short'
+    ]);
+
+    expect(anySubscriber.onChange).toBeCalledTimes(3);
+    expect(anySubscriber.onChange).toHaveBeenLastCalledWith({
+      User: { email: [], name: ['name is wrong', 'name is to short'] }
+    });
   });
 
   test('subscription lifecycle', () => {
     const service = makeErrorStoreService();
 
     // Subscribe a subscriber.
-    const subscriber = { onChange: jest.fn(), validator: 'User.email'};
+    const subscriber = { onChange: jest.fn(), validator: 'User.email' };
     service.subscribe(subscriber);
 
     // It should immediately receive the state after subscribing.
